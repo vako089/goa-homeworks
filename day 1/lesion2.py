9e51bc34389705a25dd0b1e2817dfd76c4dc883f
vako089 <vakomakharadze0412@gmail.com>
2025-02-14 20:11:53 +0400

    second comit

---
+++ day 1/lesion2.py
@@ -0,0 +1,34 @@
+name = "vako"
+#name არის ცვლადი
+# = არის ცვლადისთვის მნიშვნელოდის მიმანიწებრლი სიმბოლო
+# "vako" არის ცვლადის მნიშვნელობა
+
+surname = "makharadze"
+
+# print(name)
+#print ფუნქციას გადაეცემა ეფრანზე გამოსატანი ობიექტი
+
+name = "vako" #ეს არის str (string) ტიპის ცვლადი
+age = 16 # ეს არის int (integer) მტელი რიცხვი
+height = 177 # ეს არის float ტიპის ცვლადი ( ატწილადი )
+#boolean (bool) ტიპის ცვლადი
+
+knows_programming = True # true ან false
+is_ugly = False #snake case (უბრალოდ წერის სტილი სტანდარტულად)
+
+isUgly = False # ჯავასკრიპტული camalcase
+
+
+print(name + " " + surname)
+
+#სტრინგი არის ბრწყალებში მოქცეული სიმბოლოები
+
+
+#print(type(name))
+#print(type(age))
+#print(type(height))
+#print(type(surname))
+#print(type(knows_programming))
+
+
+print("hello my name is", name, surname, "I am", age, "years old", "my height is", str(height) + "cms.")
